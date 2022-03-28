8116478511987ff6e66f99deada77594376482fd
Joe <jmleesjc2000@gmail.com>
2022-03-27 20:49:35 -0400

    added fetching for the front end

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,11 +1,27 @@
-async function getData() {
+const submitBtn = document.querySelector('button')
+const weatherForm = document.querySelector('form')
+const messageOne = document.querySelector('.message-1')
+const messageTwo = document.querySelector('.message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+  const search = document.querySelector('#input').value
+  e.preventDefault()
+  getData(search)
+})
+
+async function getData(value) {
   try {
-    const res = await fetch("http://localhost:3000/weather?address=towson");
+    messageOne.textContent = 'loading...'
+    const res = await fetch(`http://localhost:3000/weather?address=${value}`);
     const data = await res.json()
-    console.log(data)
+    const text = `It currently feels like ${data.res.feelslike} and the temperature is ${data.res.temperature} degrees`
+    messageOne.textContent = data.place
+    messageTwo.textContent = text
+
+
   } catch (e) {
     console.log(e)
+
   }
 }
 
-getData()
